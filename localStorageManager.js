1f9029bb35aa82a1168cd775abaff5b700a68b60
Irvin Real <irvin.glez.real@hotmail.com>
2024-12-31 02:52:06 -0600

    Add start button, logo, scores (in localStorage) restart game funtionality

---
+++ localStorageManager.js
@@ -0,0 +1,9 @@
+export function setLocalStorage(key, value) {
+  return localStorage.setItem(key, value)
+}
+export function getLocalStorage(key) {
+  return localStorage.getItem(key)
+}
+export function removeLocalStorage(key) {
+  return localStorage.removeItem(key)
+}
